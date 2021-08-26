a3e61f1b920e9535189f1aa8ad6ee6dd2078d415
kaustubh-jawale <kaustubh.jawale@agiliad.com>
2021-08-26 22:25:32 +0530

    errors

--- FirstProject/Interview/admin.py
+++ FirstProject/Interview/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Candidate,Employee
+from .models import Candidate,Employee,Interviewer,Human_Resources
 admin.site.register(Employee)
 admin.site.register(Candidate)
+admin.site.register(Interviewer)
+admin.site.register(Human_Resources)
--- FirstProject/Interview/models.py
+++ FirstProject/Interview/models.py
@@ -1,13 +1,37 @@
 from django.db import models
 
+class Interviewer(models.Model):
+    id=models.AutoField(primary_key=True)
+    fname=models.CharField(max_length=50)
+    lname=models.CharField(max_length=50)
+    username=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    password=models.CharField(max_length=50)
+    password2=models.CharField(max_length=50)
+    gender=models.CharField(max_length=10)
+    def __str__(self):
+        return self.fname+" "+self.lname
+
+class Human_Resources(models.Model):
+        id = models.AutoField(primary_key=True)
+        fname = models.CharField(max_length=50)
+        lname = models.CharField(max_length=50)
+        username = models.CharField(max_length=50)
+        email = models.CharField(max_length=50)
+        password = models.CharField(max_length=50)
+        password2 = models.CharField(max_length=50)
+        gender = models.CharField(max_length=10)
+
+        def __str__(self):
+            return self.fname + " " + self.lname
 
 class Candidate(models.Model):
     candidate_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    skills= models.CharField(max_length=50)
-    experience = models.CharField(max_length=70)
+    skills = models.CharField(max_length=50)
+    experience = models.FloatField()
     day = models.CharField(max_length=70)
-    time = models.CharField(max_length=50)
+    time = models.FloatField()
 
     def __str__(self):
         return self.name
@@ -50,3 +74,4 @@ class Employee(models.Model):
 
     def __str__(self):
             return self.emp_first_name + ' ' + self.emp_last_name
+
