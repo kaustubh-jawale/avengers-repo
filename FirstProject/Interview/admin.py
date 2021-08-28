cf765c76d21a3b24d43adb8bd9b8d75003e7b6b4
kaustubh-jawale <kaustubh.jawale@agiliad.com>
2021-08-28 09:58:25 +0530

    commit

--- FirstProject/Interview/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Candidate,Employee,Interviewer,Human_Resources
-admin.site.register(Employee)
-admin.site.register(Candidate)
-admin.site.register(Interviewer)
-admin.site.register(Human_Resources)
