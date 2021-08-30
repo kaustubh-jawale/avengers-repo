6f9f735a3304f406e0b10adb1e3cf1398df352fc
kaustubh-jawale <kaustubh.jawale@agiliad.com>
2021-08-30 10:57:51 +0530

    Check

---
+++ FirstProject/Interview/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import  settings
+from django.conf.urls.static import  static
+from . import  views
+
+urlpatterns = [
+
+    path('', views.welcome,name="welcome"),
+    path('home/',views.home,name="home"),
+    path('register_both/', views.register_both, name="register_both"),
+
+    #Registration for both
+    path('hr_register', views.hr_register, name="hr_register"),
+    path('interviewer_register',views.interviewer_register,name="interviewer_register"),
+
+    #Login For for both
+    path('interviewer', views.interviewer,name="interviewer"),
+    path('hr_login', views.hr_login,name="hr_login"),
+
+
+    #After Login
+    path('time', views.time, name="time"),
+
+    path('hr', views.hr, name="hr"),
+    path('submit_candidateinfo/', views.submit_candidateinfo, name="submit"),
+
+
+    #Logout
+    path('', views.Logout, name="Logout"),
+
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
